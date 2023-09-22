0706b16c7d8cc9a8d95b502313fd62176120a611
nabilaChehla <136928671+nabilaChehla@users.noreply.github.com>
2023-09-22 12:44:04 +0200

    📝 init tic tac toe game: Broad done

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import Button from "./Button"
-import ShoppingList from "./Shopping"
-import { useState } from "react"
-
+import Board from "./Board"
+import './style.css'
 function App() {
-  const [numClick, setNumClick]= useState<number>(0)
-  
-  const handelClick  = ()=>{
-    setNumClick(numClick + 1)
-  }
-
   return (
     <>
-      <form>
-        <label className="addItem">
-          <h2>Add new item: </h2>
-          <br />
-          <input type="text" defaultValue="John" />
-        </label>
-        <button>ADD</button>
-      </form>
-      <Button count={numClick} onClick={handelClick}/>
-      <Button  count={numClick} onClick={handelClick}/>
-      <ShoppingList />
-
+    <Board />
     </>
   )
 }
