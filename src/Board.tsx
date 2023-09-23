463689b45a181be9e35175bfede7e17b283cc88f
nabilaChehla <136928671+nabilaChehla@users.noreply.github.com>
2023-09-23 15:20:12 +0200

    🚑 Modification : declaring state in the parent componant(board) to have two child components communicate with each other

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,48 +1,38 @@
 import{ useState } from 'react';
 
-let turn = 0 ; 
+type Marker = 'X' | 'O' | null;
 
-
-
-function Square({id}:{id:number}){
-  const [check, setCheck]=useState<string>('');
-
-  function handelCheck(){
-    if(check=='' && turn %2 == 0  ){
-      setCheck('X')
-      turn ++ ;
-    }
-    else if(check=='' && turn %2 == 1 ){
-     setCheck('O');
-      turn ++ ;
-  }}
-
+function Square({value,setMarkerOnClick}:{value:Marker,setMarkerOnClick:()=>void}){
   return(
-    <button className="square" onClick={handelCheck}>{check}</button>
+    <button className="square" onClick={setMarkerOnClick}>{value} </button>
   )
 }
 
 
 function Board() {
-
-
+  const [boardSquares , setboardSquares] = useState<Array<null|Marker>>(Array(9).fill(null));
+  function handelBoardSquare(i:number){
+    const squares = boardSquares.slice()// a copy of boardSquares
+    squares[i]='X'
+    setboardSquares(squares)
+  }
     return (
       <>
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={boardSquares[0]} setMarkerOnClick={()=>{handelBoardSquare(0)}}/>
+      <Square value={boardSquares[1]} setMarkerOnClick={()=>{handelBoardSquare(1)}}/>
+      <Square value={boardSquares[2]} setMarkerOnClick={()=>{handelBoardSquare(2)}}/>
       </div>
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={boardSquares[3]} setMarkerOnClick={()=>{handelBoardSquare(3)}}/>
+      <Square value={boardSquares[4]} setMarkerOnClick={()=>{handelBoardSquare(4)}}/>
+      <Square value={boardSquares[5]} setMarkerOnClick={()=>{handelBoardSquare(5)}}/>
       </div>
 
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={boardSquares[6]} setMarkerOnClick={()=>{handelBoardSquare(6)}}/>
+      <Square value={boardSquares[7]} setMarkerOnClick={()=>{handelBoardSquare(7)}}/>
+      <Square value={boardSquares[8]} setMarkerOnClick={()=>{handelBoardSquare()}}/>
       </div>
       </>
     )
